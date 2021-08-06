27075d5f8923ec0e926d94d08ecf32b74bd25aca
GhostGramm <marshalonah@gmail.com>
2021-08-06 11:57:15 +0100

    move arrow and destroy

--- Scripts/Arrow.cs
+++ Scripts/Arrow.cs
@@ -5,6 +5,7 @@ public class Arrow : Node2D
 {
    
     private float speed = 100f;
+    private float lifeSpan =3f;
     public override void _Ready()
     {
         
@@ -13,7 +14,22 @@ public class Arrow : Node2D
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        moveArrow(delta);
+    }
+
+    public void moveArrow(float delta)
+    {
         GD.Print("arrow movinig");
-        Position = Transform.x * delta * speed;
+        Position += Transform.x * delta * speed;
+        lifeSpan -= delta;
+        if(lifeSpan <= 0)
+        {
+            destroyArrow();
+        }
+    }
+
+    public void destroyArrow()
+    {
+        QueueFree();
     }
 }
